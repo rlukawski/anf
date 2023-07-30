40e763158e88d37838551a519eab8b6b1fe4ac93
Rafał Łukawski <rs.lukawski@gmail.com>
2023-07-30 09:24:19 +0200

    add exhautiveness homework

--- webapp/src/lessons/m1/snippets/unions.3.exhaustiveness.HOMEWORK.ts
+++ webapp/src/lessons/m1/snippets/unions.3.exhaustiveness.HOMEWORK.ts
@@ -11,20 +11,36 @@
 
 type Country = 'PL' | 'DE' | 'FR' | 'UK'
 
-type Tax_PL = 0 | 0.05 | 0.08 | 0.23 // 0%, 5%, 8%, 23%
-type Tax_DE = 0 | 0.07 | 0.19 // 0%, 7%, 19%
+type Tax_PL = 0 | 0.055 | 0.08 | 0.23 // 0%, 5%, 8%, 23%
+type Tax_DE = 0.1 | 0.07 | 0.19 // 0%, 7%, 19%
 type Tax_FR = 0 | 0.021 | 0.055 | 0.1 | 0.2 // 0%, 2.1%, 5.5%, 10%, 20%
-type Tax_UK = 0 | 0.05 | 0.20 // 0%, 5%, 20%
+type Tax_UK = 0 | 0.05 | 0.23 // 0%, 5%, 20%
 
 // ten typ jest za szeroki...
 // bo pozwala na połączenie dowolnej stawki podatku z dowolnym krajem
 // a chcemy, aby dopuszczalne były tylko te
-type Purchase = {
-  country: Country
-  vatTax: number
-  name: string
-  netPrice: number
+// type Purchase = {
+//   country: Country
+//   vatTax: number
+//   name: string
+//   netPrice: number
+// }
+
+type PrePurchase = {
+  country: 'PL'
+  vatTax: Tax_PL
+} | {
+  country: 'DE'
+  vatTax: Tax_DE
+} | {
+  country: 'FR'
+  vatTax: Tax_FR
+} | {
+  country: 'UK'
+  vatTax: Tax_UK
 }
+
+type Purchase = PrePurchase & { name: string; netPrice: number }
 
 const purchases: Purchase[] = [{
   country: 'PL',
