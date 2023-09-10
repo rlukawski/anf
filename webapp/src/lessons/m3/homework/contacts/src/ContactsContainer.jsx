c547224b2b48d0605afb9ea739420e0419f890d2
Rafał Łukawski <rs.lukawski@gmail.com>
2023-09-10 16:56:25 +0200

    fix tests

--- webapp/src/lessons/m3/homework/contacts/src/ContactsContainer.jsx
+++ webapp/src/lessons/m3/homework/contacts/src/ContactsContainer.jsx
@@ -1,92 +1,82 @@
 /* eslint-disable */
-import React from 'react';
+import React, { useEffect } from "react";
 
-import { ContactList } from './ContactList';
-import { ContactForm } from './ContactForm';
+import { ContactList } from "./ContactList";
+import { ContactForm } from "./ContactForm";
 
-export class ContactsContainer extends React.Component {
+export const ContactsContainer = (props) => {
+  const serviceRef = React.useRef(null);
+  const [contacts, setContacts] = React.useState([]);
+  const [selected, setSelected] = React.useState(null);
 
-  constructor(props) {
-    super(props);
+  useEffect(() => {
+    serviceRef.current = props.service;
+    serviceRef.current.getContacts().then((contacts) => {
+      setContacts(contacts);
+    });
+  }, []);
 
-    // ContactsService object
-    this.service = this.props.service;
+  const newContact = () => {
+    setSelected({
+      name: "",
+      details: "",
+    });
+  };
 
-    this.state = {
-      contacts: [],
-      selected: null
-    };
+  const onSelect = (contact) => {
+    setSelected(contact);
+  };
 
-    this.service.getContacts().then((contacts) => {
-      this.setState({
-        contacts
+  const onSubmit = (contact) => {
+    serviceRef.current.saveContact(contact).then(() => {
+      serviceRef.current.getContacts().then((contacts) => {
+        setContacts(contacts);
       });
     });
-  }
+  };
 
-  newContact = () => {
+  const onCancel = () => {
     this.setState({
-      selected: {
-        name: '',
-        details: ''
-      }
+      selected: null,
     });
-  }
+  };
 
-  onSelect = (contact) => {
-    this.setState({
-      selected: contact
-    });
-  }
-
-  onSubmit = (contact) => {
-    this.service.saveContact(contact).then(()=>{
-      this.service.getContacts().then((contacts) => {
-        this.setState({
-          contacts
-        });
-      });
-    });
-  }
-
-  onCancel = () => {
-    this.setState({
-      selected: null
-    });
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <h1>Contacts</h1>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-4">
-            <ContactList
-              contacts={this.state.contacts}
-              selected={this.state.selected}
-              onSelect={this.onSelect}
-            />
-          </div>
-          <div className="col-md-4">
-            {this.state.selected
-            ? (
-              <ContactForm contact={this.state.selected}
-                onChange={this.onSelect}
-                onSubmit={this.onSubmit}
-                onCancel={this.onCancel}
-              />
-            ) : (
-              <div>
-                <button id="new-contact" onClick={this.newContact} className="btn btn-primary">New contact</button>
-              </div>
-            )}
-          </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <h1>Contacts</h1>
         </div>
       </div>
-    );
-  }
-}
+      <div className="row">
+        <div className="col-md-4">
+          <ContactList
+            contacts={contacts}
+            selected={selected}
+            onSelect={onSelect}
+          />
+        </div>
+        <div className="col-md-4">
+          {selected ? (
+            <ContactForm
+              contact={selected}
+              onChange={onSelect}
+              onSubmit={onSubmit}
+              onCancel={onCancel}
+            />
+          ) : (
+            <div>
+              <button
+                id="new-contact"
+                onClick={newContact}
+                className="btn btn-primary"
+              >
+                New contact
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
